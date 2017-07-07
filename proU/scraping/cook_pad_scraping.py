873b8c895600e26a8d878b4ffe61460e07e63e58
Nekonoha <tan.fantazma@gmail.com>
2017-07-07 15:02:57 +0900

    sintyoku

--- proU/scraping/cook_pad_scraping.py
+++ proU/scraping/cook_pad_scraping.py
@@ -12,16 +12,28 @@ text = res.text
 soup = BeautifulSoup(text, "html.parser")
 
 recipes = soup.select(".clearfix")
-steps = soup.select(".step")
+# steps = soup.select(".step,.step_last")
+steps = soup.select(".instruction")
 step_texts = soup.select(".step_text")
 
+
 f = codecs.open("steps.html", "w", "utf-8")
-for step in steps:
+
+for i, step in enumerate(steps):
+    i += 1
+    f.write("<h3 id=step" + str(i) + ">step" + str(i) + "</h3>")
     f.write(str(step))
+    f.write("</div>")
     f.write("<br>")
+    f.write("<a href=#step" + str(i-1) + "><button type='button'>prev</button></a>")
+    f.write("<a href=#step" + str(i+1) + "><button type='button'>next</button></a>")
 f.close()
 
 f = codecs.open("recipes.html", "w", "utf-8")
 f.write(str(recipes[0]))
 f.write("<br>")
 f.close()
+
+
+print(steps)
+print()
